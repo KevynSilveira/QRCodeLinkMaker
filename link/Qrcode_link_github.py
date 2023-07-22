b688ac3f35e415f3a831e1d8c431bbd78deaaed1
Kevyn <kevynsilveira0610@gmail.com>
2023-07-21 21:36:18 -0300

    alteracao nas funcao

--- link/Qrcode_link_github.py
+++ link/Qrcode_link_github.py
@@ -1,26 +1,13 @@
 import qrcode # pip install qrcode
 import customtkinter as ctk # pip install customtkinter
+from tkinter import filedialog
+from tkinter import messagebox
+import tkinter as tk
 
 link = ""
 generated_qrcode_path = ""
 
-image = qrcode.make("https://github.com/KevynSilveira") # Controi o qrcode
-image.save("qr_code_github.png") # Salva ele
-
-
-# Função para converter o arquivo
-def converter():
-    # Obter o caminho do arquivo da entrada de texto
-    file_path = caminho_arquivo.get()
-    print(file_path)
-
-    # Verificar se foi selecionado um arquivo
-    if not file_path:
-        print("Nenhum arquivo selecionado.")
-        return
-
 def create_interface():
-
 
     main_frame = ctk.CTk()    # Criando frame principal
     main_frame.geometry("600x300")
@@ -33,19 +20,42 @@ def create_interface():
     c_link = ctk.CTkEntry(master=main_frame, width=200, height=30, corner_radius=8)
     c_link.place(x=10, y=80)
 
+    def select_path():  # Seleciona a pasta para salvar o arquivo
+        pasta_selecionada = filedialog.askdirectory(title="Selecione uma pasta")  # Seleciona a pasta para salvar
+        if pasta_selecionada:
+            global generated_qrcode_path
+            generated_qrcode_path = pasta_selecionada
+            c_path.insert(tk.END, pasta_selecionada)
+            return pasta_selecionada
+        else:
+            return None
+            messagebox.showerror("Caminho destino", "Selecione um arquivo!")
+
+    def create_qrcode():
+        try:
+            global generated_qrcode_path, link
+            link = c_link.get()
+            select_path()
+            if c_link.get() != "" and generated_qrcode_path != "":
+                image = qrcode.make(link)  # Controi o qrcode
+                image.save(generated_qrcode_path)  # Salva ele no local selecionado
+                messagebox.showinfo("QR code", "Seu QR code foi gerado")
+            else:
+                messagebox.showerror("Atencao", "Atencao, preencha todos os campos!")
+        except:
+            messagebox.showerror("Atencao", "Dados invalidos!")
+
     l_path = ctk.CTkLabel(master=main_frame, text="Insira a URL de destino:")
     l_path.place(x=45, y=110)
 
     c_path = ctk.CTkEntry(master=main_frame, width=200, height=30, corner_radius=8)
     c_path.place(x=10, y=140)
 
-    b_create = ctk.CTkButton(master=main_frame, width=200, height=30, text="Create")
-    b_create.place(x=10, y=190)
+    b_create = ctk.CTkButton(master=main_frame, width=200, height=30, text="Create", command=create_qrcode)
+    b_create.place(x=10, y=180)
 
     qrcode_frame = ctk.CTkFrame(master=main_frame, width=370, height= 280)
     qrcode_frame.place(x=220, y=10)
-
-
 
 
 
